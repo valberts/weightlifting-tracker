2da2044afcd69ef100887aac321ee552e314bb1d
Vincent Albertsson <albertsson.vincent@gmail.com>
2022-11-24 23:29:26 +0900

    Added dashboard page and PWA support

--- pages/_document.js
+++ pages/_document.js
@@ -14,7 +14,12 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>{CssBaseline.flush()}</Head>
+        <Head>
+          {CssBaseline.flush()}
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/icon.png"></link>
+          <meta name="theme-color" content="#fff" />
+        </Head>
         <body>
           <Main />
           <NextScript />
