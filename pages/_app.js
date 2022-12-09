af218c8aac5347fed7c93ac4a2f8ce567d9f82ca
Vincent Albertsson <albertsson.vincent@gmail.com>
2022-12-09 20:58:38 +0900

    Fresh Next.js app, with tailwind

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,7 @@
-// 1. import `NextUIProvider` component
-import { NextUIProvider } from '@nextui-org/react';
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return (
-    // 2. Use at the root of your app
-    <NextUIProvider>
-      <Component {...pageProps} />
-    </NextUIProvider>
-  );
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
