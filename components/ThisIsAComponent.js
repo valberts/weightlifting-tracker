24395f8f3866b3deca7c64bd0b3f3616bffeca50
Vincent Albertsson <albertsson.vincent@gmail.com>
2022-12-22 01:31:10 +0900

    Deleted testing component

--- components/ThisIsAComponent.js
+++ components/ThisIsAComponent.js
@@ -16,40 +16,19 @@ const Selector = () => {
             })
     }, [])
     return (
-        <div className="w-72 font-medium h-80">
+        <div className="w-full text-black">
             <div
                 onClick={() => setOpen(!open)}
-                className={`bg-white w-full p-2 flex items-center justify-between rounded ${
-                    !selected && 'text-gray-700'
-                }`}
+                className={`bg-white w-full p-2 flex items-center justify-between`}
             >
-                {selected
-                    ? selected?.length > 25
-                        ? selected?.substring(0, 25) + '...'
-                        : selected
-                    : 'Select Country'}
-                {/* <BiChevronDown
-                    size={20}
-                    className={`${open && 'rotate-180'}`}
-                /> */}
+                Select exercise
             </div>
             <ul
-                className={`bg-white mt-2 overflow-y-auto ${
+                className={`bg-white overflow-y-auto ${
                     open ? 'max-h-60' : 'max-h-0'
                 } `}
             >
-                <div className="flex items-center px-2 sticky top-0 bg-white">
-                    <AiOutlineSearch size={18} className="text-gray-700" />
-                    <input
-                        type="text"
-                        value={inputValue}
-                        onChange={(e) =>
-                            setInputValue(e.target.value.toLowerCase())
-                        }
-                        placeholder="Enter country name"
-                        className="placeholder:text-gray-700 p-2 outline-none"
-                    />
-                </div>
+                <div className="flex items-center px-2 sticky top-0 bg-white"></div>
                 {countries?.map((country) => (
                     <li
                         key={country?.name}
